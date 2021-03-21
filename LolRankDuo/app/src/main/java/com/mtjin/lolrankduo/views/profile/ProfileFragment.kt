d5e06c51f5e2b932976140e24ccd377fa95c403f
jackjackE <tmddjs210@naver.com>
2021-03-21 20:24:19 +0900

    [Add] ProfileFragment.kt 추가

---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/profile/ProfileFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.lolrankduo.views.profile
+
+import com.mtjin.lolrankduo.R
+import com.mtjin.lolrankduo.base.BaseFragment
+import com.mtjin.lolrankduo.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun init() {
+
+    }
+}
