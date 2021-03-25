3e3c0392548271b7dd8ca3b2ebf2f94ea399ad14
jackjackE <tmddjs210@naver.com>
2021-03-25 21:22:11 +0900

    [Implement] 프로필 공백 토스트 메세지 구현

--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/profile/ProfileFragment.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/profile/ProfileFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.provider.MediaStore
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.lifecycle.Observer
 import com.mtjin.lolrankduo.R
 import com.mtjin.lolrankduo.base.BaseFragment
 import com.mtjin.lolrankduo.databinding.FragmentProfileBinding
@@ -14,7 +13,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
     private val viewModel: ProfileViewModel by viewModel()
-    private val getContent =
+    private val getImage =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             binding.ivProfileImage.setImageURI(result.data?.data)
         }
@@ -27,11 +26,19 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
 
     private fun initViewModelCallback() {
         with(viewModel) {
-            pickImage.observe(this@ProfileFragment, Observer {
+            pickImage.observe(this@ProfileFragment, {
                 val intent = Intent(Intent.ACTION_PICK)
                 intent.type = MediaStore.Images.Media.CONTENT_TYPE
                 intent.type = "image/*"
-                getContent.launch(intent)
+                getImage.launch(intent)
+            })
+
+            gameIdEmpty.observe(this@ProfileFragment, {
+                showToast("게임 아이디를 적어주세요")
+            })
+
+            ageEmpty.observe(this@ProfileFragment, {
+                showToast("나이를 적어주세요")
             })
         }
     }
