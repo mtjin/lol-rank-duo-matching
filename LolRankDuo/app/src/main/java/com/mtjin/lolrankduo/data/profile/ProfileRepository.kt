decd22a11faf0f1e94b1fc6d30ef252f8cef37b9
jackjackE <tmddjs210@naver.com>
2021-03-26 22:51:22 +0900

    [Implement] ProfileRepository.kt uploadProfileImage() 구현

--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/profile/ProfileRepository.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/profile/ProfileRepository.kt
@@ -1,4 +1,8 @@
 package com.mtjin.lolrankduo.data.profile
 
+import android.net.Uri
+import io.reactivex.rxjava3.core.Single
+
 interface ProfileRepository {
+    fun uploadProfileImage(imageUri: Uri): Single<String>
 }
--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/profile/ProfileRepositoryImpl.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/profile/ProfileRepositoryImpl.kt
@@ -1,9 +1,33 @@
 package com.mtjin.lolrankduo.data.profile
 
+import android.net.Uri
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
+import com.mtjin.lolrankduo.utils.UserInfo
+import com.mtjin.lolrankduo.utils.constants.ERR_UPLOAD_IMAGE
+import io.reactivex.rxjava3.core.Single
 
 class ProfileRepositoryImpl(
     private val db: FirebaseFirestore,
     private val storage: StorageReference
-) : ProfileRepository
+) : ProfileRepository {
+    override fun uploadProfileImage(imageUri: Uri): Single<String> {
+        return Single.create { emitter ->
+            val storageRef = storage.child("${UserInfo.uuid}.png")
+            val uploadTask = storageRef.putFile(imageUri)
+            uploadTask.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let { throw it }
+                }
+                storageRef.downloadUrl
+            }.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result.toString()
+                    emitter.onSuccess(downloadUri)
+                } else {
+                    emitter.onError(Throwable(ERR_UPLOAD_IMAGE))
+                }
+            }
+        }
+    }
+}
