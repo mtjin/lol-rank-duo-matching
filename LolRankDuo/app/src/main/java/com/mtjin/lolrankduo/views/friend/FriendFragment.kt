e629aba4ebdb8169c1f968fc15e51a68c3a7d5e6
jackjackE <tmddjs210@naver.com>
2021-03-29 21:13:28 +0900

    [Add] FriendFragment.kt 추가

---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/friend/FriendFragment.kt
@@ -0,0 +1,13 @@
+package com.mtjin.lolrankduo.views.friend
+
+import com.mtjin.lolrankduo.R
+import com.mtjin.lolrankduo.base.BaseFragment
+import com.mtjin.lolrankduo.databinding.FragmentFriendBinding
+
+
+class FriendFragment : BaseFragment<FragmentFriendBinding>(R.layout.fragment_friend) {
+    override fun init() {
+        
+    }
+
+}
