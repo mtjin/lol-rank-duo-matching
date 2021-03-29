29d04aed5f22d80ac377b5de59231816bdb4dbeb
jackjackE <tmddjs210@naver.com>
2021-03-29 20:46:24 +0900

    [Add] ChatFragment.kt 네비게이션 그래프 추가

---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/chat/ChatFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.lolrankduo.views.chat
+
+import com.mtjin.lolrankduo.R
+import com.mtjin.lolrankduo.base.BaseFragment
+import com.mtjin.lolrankduo.databinding.FragmentChatBinding
+
+class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
+    override fun init() {
+        
+    }
+}
