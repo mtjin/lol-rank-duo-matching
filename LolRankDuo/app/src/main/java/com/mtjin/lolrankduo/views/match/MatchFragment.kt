44bfbbeba8a0ba817fd315d6d1a442bee111f785
jackjackE <tmddjs210@naver.com>
2021-04-19 15:11:51 +0900

    [Implement] MatchFragment.kt 툴바 구현

--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/match/MatchFragment.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/match/MatchFragment.kt
@@ -1,5 +1,6 @@
 package com.mtjin.lolrankduo.views.match
 
+import android.view.View
 import com.mtjin.lolrankduo.R
 import com.mtjin.lolrankduo.base.BaseFragment
 import com.mtjin.lolrankduo.databinding.FragmentMatchBinding
@@ -7,6 +8,9 @@ import com.mtjin.lolrankduo.databinding.FragmentMatchBinding
 
 class MatchFragment : BaseFragment<FragmentMatchBinding>(R.layout.fragment_match) {
     override fun init() {
-
+        binding.toolbar.run {
+            tbToolbar.title = getString(R.string.duo_matching_text)
+            ivEdit.visibility = View.GONE
+        }
     }
 }
