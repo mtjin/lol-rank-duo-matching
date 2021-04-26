1f8453dd535839b3e7a3a3cb4a56188f09e2e783
jackjackE <tmddjs210@naver.com>
2021-04-26 19:25:12 +0900

    [feat] MatchRepository.kt 프로필 불러오기

--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/match/MatchRepository.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/match/MatchRepository.kt
@@ -5,4 +5,5 @@ import io.reactivex.rxjava3.core.Single
 
 interface MatchRepository {
     fun requestDuoMatch(user: User): Single<User>
+    fun requestProfile() : Single<User>
 }
--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/match/MatchRepositoryImpl.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/match/MatchRepositoryImpl.kt
@@ -1,12 +1,37 @@
 package com.mtjin.lolrankduo.data.match
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mtjin.lolrankduo.data.models.User
+import com.mtjin.lolrankduo.utils.UserInfo
+import com.mtjin.lolrankduo.utils.constants.DB_USER
+import com.mtjin.lolrankduo.utils.constants.TAG
 import io.reactivex.rxjava3.core.Single
 
-class MatchRepositoryImpl : MatchRepository {
-    override fun requestDuoMatch(user : User): Single<User> {
+class MatchRepositoryImpl(private val db: FirebaseFirestore) : MatchRepository {
+    override fun requestDuoMatch(user: User): Single<User> {
         return Single.create { emitter ->
 
+        }
+    }
+
+    override fun requestProfile(): Single<User> {
+        return Single.create { emitter ->
+            db.collection(DB_USER)
+                .document(UserInfo.profile.id)
+                .addSnapshotListener { snapshot, e ->
+                    if (e != null) {
+                        Log.w(TAG, "Listen failed.", e)
+                        return@addSnapshotListener
+                    }
+
+                    if (snapshot != null && snapshot.exists()) {
+                        Log.d(TAG, "Current data: ${snapshot.data}")
+                        emitter.onSuccess(snapshot.toObject(User::class.java))
+                    } else {
+                        Log.d(TAG, "Current data: null")
+                    }
+                }
         }
     }
 }
