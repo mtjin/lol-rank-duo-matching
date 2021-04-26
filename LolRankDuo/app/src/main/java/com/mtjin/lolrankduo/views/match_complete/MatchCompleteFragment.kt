7c4b30934d2dd6c0fc4723e0cfe97772e9097032
jackjackE <tmddjs210@naver.com>
2021-04-26 20:51:03 +0900

    [Feature] MatchCompleteFragment.kt 추가

---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/views/match_complete/MatchCompleteFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.lolrankduo.views.match_complete
+
+import com.mtjin.lolrankduo.R
+import com.mtjin.lolrankduo.base.BaseFragment
+import com.mtjin.lolrankduo.databinding.FragmentMatchCompleteBinding
+
+class MatchCompleteFragment :
+    BaseFragment<FragmentMatchCompleteBinding>(R.layout.fragment_match_complete) {
+    override fun init() {
+
+    }
+}
