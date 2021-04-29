d0919db71ad862dc88caa483d1853f2e3ba9b6c3
jackjackE <tmddjs210@naver.com>
2021-04-29 23:55:10 +0900

    [Revise] User.kt 티어 String -> Int 로 변경

--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/models/User.kt
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/models/User.kt
@@ -5,13 +5,13 @@ data class User(
     var id: String = "", // 고유값 (카카오로그인 이메일로 할듯)
     var gameId: String = "", // 게임 아이디
     var profileImage: String = "", // 프로필 이미지 url
-    var positionList: List<String> = ArrayList(), // 내 포지션
+    var positionList: List<Int> = ArrayList(), // 내 포지션
     var sex: String = "", // 성별
     var tear: String = "",  // 티어
     var age: String = "", // 나이
     var introduce: String = "", // 소개글
     var lastLoginTimestamp: Long = 0, //마지막 접속 시간
-    var teamPositionList: List<String> = ArrayList(), //내가 원하는 팀원 포지션
+    var teamPositionList: List<Int> = ArrayList(), //내가 원하는 팀원 포지션
     var voice: Boolean = false, //보이스 가능 여부
     var fcm: String = "", // FCM 토큰
     var historyIdList: List<String> = ArrayList(), // 이미 나와 이어진 아이디들 (재매칭안되게 하기위해)
