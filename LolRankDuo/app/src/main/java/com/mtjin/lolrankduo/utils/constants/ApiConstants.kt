59d3830b338ad1e5e7b9f6fa356d3e1b0c3996ce
jackjackE <tmddjs210@naver.com>
2021-05-10 22:49:19 +0900

    [Feat] ApiConstants.kt 추가

---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/utils/constants/ApiConstants.kt
@@ -0,0 +1,5 @@
+package com.mtjin.lolrankduo.utils.constants
+
+const val FCM_NAMED = "FCM_NAMED"
+const val TOUR_NAMED = "TOUR_NAMED"
+const val FCM_KEY = "AAAA_mSKDps:APA91bHD7uxADiQB7qd1x-T2Ejgf2IvLKCvKi05P4asFMefTCASLGYBLi0LZ9pXBPrTDZ_LEag_YMIEozX464gkBLEvZjYbbxYRZQZlF9qHk0mju_djXiKo4EszhXM0cfcFd_3IhCQx0"
--- LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/utils/constants/ModuleConstants.kt
+++
@@ -1,4 +0,0 @@
-package com.mtjin.lolrankduo.utils.constants
-
-const val FCM_NAMED = "FCM_NAMED"
-const val TOUR_NAMED = "TOUR_NAMED"
