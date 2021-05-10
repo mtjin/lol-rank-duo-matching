1153816b934e34b09d25c6803a740dd86cd8cac5
jackjackE <tmddjs210@naver.com>
2021-05-10 22:41:20 +0900

    [Feat] 노티피케이션 관련 data class 추가

---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/models/NotificationBody.kt
@@ -0,0 +1,3 @@
+package com.mtjin.lolrankduo.data.models
+
+data class NotificationBody(val to: String, val data: NotificationData)
---
+++ LolRankDuo/app/src/main/java/com/mtjin/lolrankduo/data/models/NotificationData.kt
@@ -0,0 +1,7 @@
+package com.mtjin.lolrankduo.data.models
+
+data class NotificationData(
+    val title: String,
+    val message: String,
+    val uuid: String
+)
